7f37eee06c33b5b4f2666d6bb0fea026851fcc5f
Andrew Zak <andrew@loadbalancer.org>
2017-09-07 12:55:21 +0100

    added leave cli command

--- cmd/commands/leave.go
+++ cmd/commands/leave.go
@@ -1,18 +1,26 @@
 package commands
 
 import (
-	"github.com/mitchellh/cli"
 	"strings"
 	"flag"
+	"github.com/mitchellh/cli"
 )
 
-type CreateCommand struct {
+package commands
+
+import (
+"github.com/mitchellh/cli"
+"strings"
+"flag"
+)
+
+type LeaveCommand struct {
 	Ui cli.Ui
 }
 
-func (c *CreateCommand) Help() string {
+func (c *LeaveCommand) Help() string {
 	helpText := `
-Usage: PulseHA create [options] address ...
+Usage: PulseHA leave [options] ...
   Tells a running PulseHA agent to join the cluster
   by specifying at least one existing member.
 Options:
@@ -20,13 +28,13 @@ Options:
 	return strings.TrimSpace(helpText)
 }
 
-func (c *CreateCommand) Run(args []string) int {
+func (c *LeaveCommand) Run(args []string) int {
 	cmdFlags := flag.NewFlagSet("create", flag.ContinueOnError)
 	cmdFlags.Usage = func() { c.Ui.Output(c.Help()) }
 
 	return 0
 }
 
-func (c *CreateCommand) Synopsis() string {
+func (c *LeaveCommand) Synopsis() string {
 	return "Tell Pulse to create new HA cluster"
 }
